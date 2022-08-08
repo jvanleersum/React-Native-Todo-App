45f7b9d5b9bda18e41a4935ccc22f9e82361bc58
Judith van Leersum <judithvleersum@gmail.com>
2022-08-08 14:37:00 +0200

    using useState and eventHandlers to get the entered text of the user and later on add this to the list of goals"

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+
+  const goalInputHandler = (text) => {
+    setEnteredText(text);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredText);
+    // add goal to list
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title="Add goal"/>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of course goals...</Text>
@@ -19,25 +38,25 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC'
+    borderBottomColor: "#CCCCCC",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#CCCCCC',
-    width: '70%', 
+    borderColor: "#CCCCCC",
+    width: "70%",
     marginRight: 8,
-    padding: 8
+    padding: 8,
   },
   goalsContainer: {
-    flex: 6
-  }
+    flex: 6,
+  },
 });
