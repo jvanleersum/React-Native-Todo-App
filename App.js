a88a4bc4f26a0e6f3ef8f527f20a1df836f54aaa
Judith van Leersum <judithvleersum@gmail.com>
2022-08-08 14:07:58 +0200

    Created basic input field and button

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of course goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  appContainer: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '80%', 
+    marginRight: 8,
+    padding: 8
+  }
 });
